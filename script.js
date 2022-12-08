3ce4ad46860164f07ed7831abf94bca023e75ec6
Sebastian Ayala <cesar.ayala1305@gmail.com>
2022-12-08 19:58:29 -0300

    Vista Previa Video

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("page loaded...");
+
+function playVideo(vid) {
+    vid.play();
+}
+
+function pauseVideo(vid) {
+    vid.pause();
+    vid.currentTime = 0;
+}
+
+function playVideoP(vid) {
+    vid.play();
+}
+
+function pauseVideoP(vid) {
+    vid.pause();
+    vid.currentTime = 0;
+}
